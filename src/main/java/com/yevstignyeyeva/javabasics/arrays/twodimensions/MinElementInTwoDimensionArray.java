8374fc7320e7de03b37d2a077c26c05171b3d290
nayanzin-alexander <nayanzin.alexander@gmail.com>
2020-09-27 19:18:30 +0300

    Arrays 2D array find min element algorithm

---
+++ src/main/java/com/yevstignyeyeva/javabasics/arrays/twodimensions/MinElementInTwoDimensionArray.java
@@ -0,0 +1,13 @@
+package com.yevstignyeyeva.javabasics.arrays.twodimensions;
+
+public class MinElementInTwoDimensionArray {
+    public static void main(String[] args) {
+        // TODO init 2D array
+
+        // TODO invoke getMinElement(array)
+
+        // TODO print tgiven array and the min element
+    }
+
+    // TODO create getMinElement(array) method
+}
